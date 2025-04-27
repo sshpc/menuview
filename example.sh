0f0bd2518ccc95ce876264fb528ebd5f0cd58583
sshpc <sshpc@qq.com>
2025-04-27 06:19:55 +0000

    250427061955

--- example.sh
+++ example.sh
@@ -1,122 +1,45 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
 #全局变量初始化
-sinit() {
-  version='0.2.0'
-  #菜单名称(默认主页)
-  menuname='主页'
-}
-_blue() {
-  printf '\033[0;31;36m%b\033[0m' "$1"
-  echo
-}
-_yellow() {
-  printf '\033[0;31;33m%b\033[0m' "$1"
-  echo
-}
-#菜单头部
-menutop() {
-  clear
-  echo
-  _blue ">~~~~~~~~~~~~~~ xxx ~~~~~~~~~~~~<  版本:v$version"
-  echo
-  _yellow "当前菜单: $menuname "
-  echo
-}
-#菜单底部
-menubottom() {
-  echo
-  printf '\033[0;31;36m%b\033[0m' "q: 退出  "
-  if [[ "$number" != "" ]]; then printf '\033[0;31;36m%b\033[0m' "  0: 返回主页"; fi
-  echo
-  echo
-  read -ep "请输入命令数字: " number
-}
+version='0.3'
 
-menu() {
-  menutop
-        options=("$@")
-        num_options=${#options[@]}
-        # 计算数组中的字符最大长度
-        max_len=0
-        for ((i = 0; i < num_options; i++)); do
-            # 获取当前字符串的长度
-            str_len=${#options[i]}
+source menu.sh
 
-            # 更新最大长度
-            if ((str_len > max_len)); then
-                max_len=$str_len
-            fi
-        done
-        # 渲染菜单
-        for ((i = 0; i < num_options; i += 4)); do
-            printf "%s%*s  " "$((i / 2 + 1)): ${options[i]}" "$((max_len - ${#options[i]}))"
-            if [[ "${options[i + 2]}" != "" ]]; then printf "$((i / 2 + 2)): ${options[i + 2]}"; fi
-            echo
-            echo
-        done
-        echo
-        printf '\033[0;31;36m%b\033[0m' "q: 退出  "
-        if [[ "$number" != "" ]]; then printf '\033[0;31;36m%b\033[0m' "b: 返回  0: 首页"; fi
-        echo
-        echo
-        # 获取用户输入
-        read -ep "请输入命令号: " number
-        if [[ $number -ge 1 && $number -le $((num_options / 2)) ]]; then
-            #找到函数名索引
-            action_index=$((2 * (number - 1) + 1))
-            #函数名赋值
-            parentfun=${options[action_index]}
-            #函数执行
-            ${options[action_index]}
-            #执行完后自动返回
-            waitinput
-            ${FUNCNAME[3]}
-        elif [[ $number == 0 ]]; then
-            main
-        elif [[ $number == 'b' ]]; then
-            ${FUNCNAME[3]}
-        elif [[ $number == 'q' ]]; then
-            echo
-            exit
-        else
-            echo
-            _red '输入有误  回车返回首页'
-            waitinput
-            main
-        fi
-}
 options1() {
+  options1-1() {
+    echo
+    echo "this is options1-1"
+    echo
+  }
   menuname='主页/选项1'
-  options=("选项11" options1 "选项12" options2 "选项13" options3)
+  options=("选项1-1" options1-1 "选项1-2" options1-2 "选项1-3" options1-3)
 
   menu "${options[@]}"
 }
 
+options2-1() {
+  echo
+  echo "this is options2-1"
+  echo
+}
+
 options2() {
   menuname='主页/选项2'
-  options=("选项21" options1 "选项22" options2)
+  options=("选项2-1" options2-1 "选项2-2" options2-2)
 
   menu "${options[@]}"
 }
 
 options3() {
-  menuname='主页/选项3'
-  options=("选项31" options1)
+  echo
+  echo "this is options3"
+  echo
 
-  menu "${options[@]}"
-}
-
-options4() {
-  menuname='主页/选项4'
-  options=("选项41" options1 "选项42" options2 "选项43" options3)
-
-  menu "${options[@]}"
 }
 
 main() {
   #定义数组
-  options=("选项1" options1 "选项2" options2 "选项3" options3 "选项4" options4)
+  options=("选项1" options1 "选项2" options2 "选项3" options3)
 
   #调用函数
   menu "${options[@]}"
--- menu.sh
+++ menu.sh
@@ -1,61 +1,71 @@
-#!/bin/bash
-export LANG=en_US.UTF-8
-
-menu() {
-        options=("$@")
-        num_options=${#options[@]}
-        # 计算数组中的字符最大长度
-        max_len=0
-        for ((i = 0; i < num_options; i++)); do
-            # 获取当前字符串的长度
-            str_len=${#options[i]}
-
-            # 更新最大长度
-            if ((str_len > max_len)); then
-                max_len=$str_len
-            fi
-        done
-        # 渲染菜单
-        for ((i = 0; i < num_options; i += 4)); do
-            printf "%s%*s  " "$((i / 2 + 1)): ${options[i]}" "$((max_len - ${#options[i]}))"
-            if [[ "${options[i + 2]}" != "" ]]; then printf "$((i / 2 + 2)): ${options[i + 2]}"; fi
-            echo
-            echo
-        done
-        echo
-        printf '\033[0;31;36m%b\033[0m' "q: 退出  "
-        if [[ "$number" != "" ]]; then printf '\033[0;31;36m%b\033[0m' "b: 返回  0: 首页"; fi
-        echo
-        echo
-        # 获取用户输入
-        read -ep "请输入命令号: " number
-        if [[ $number -ge 1 && $number -le $((num_options / 2)) ]]; then
-            #找到函数名索引
-            action_index=$((2 * (number - 1) + 1))
-            #函数名赋值
-            parentfun=${options[action_index]}
-            #函数执行
-            ${options[action_index]}
-            #执行完后自动返回
-            waitinput
-            ${FUNCNAME[3]}
-        elif [[ $number == 0 ]]; then
-            main
-        elif [[ $number == 'b' ]]; then
-            ${FUNCNAME[3]}
-        elif [[ $number == 'q' ]]; then
-            echo
-            exit
-        else
-            echo
-            _red '输入有误  回车返回首页'
-            waitinput
-            main
-        fi
+#菜单名称(默认主页)
+menuname='主页'
+# 颜色定义
+_red() {
+    printf '\033[0;31;31m%b\033[0m' "$1"
+    echo
+}
+_green() {
+    printf '\033[0;31;32m%b\033[0m' "$1"
+    echo
+}
+_yellow() {
+    printf '\033[0;31;33m%b\033[0m' "$1"
+    echo
+}
+_blue() {
+    printf '\033[0;31;36m%b\033[0m' "$1"
+    echo
 }
 
-#定义数组
-options=("选择options1" options1 "选择options2" options2 "选择options3" options3)
+# 等待输入
+waitinput() {
+    echo
+    read -n1 -r -p "按任意键继续...(退出 Ctrl+C)"
+}
 
-#调用函数
-menu "${options[@]}"
+
+menu() {
+    clear
+    _green '>~~~~~~~~~~~~~~菜单列表~~~~~~~~~~~~~~<'
+    echo
+    _yellow "当前菜单: $menuname "
+    echo
+
+    local options=("$@")
+    local num_options=${#options[@]}
+    local max_len=0
+
+    for ((i = 0; i < num_options; i += 2)); do
+        local str_len=${#options[i]}
+        ((str_len > max_len)) && max_len=$str_len
+    done
+
+    for ((i = 0; i < num_options; i += 4)); do
+        printf "%s%*s  " "$((i / 2 + 1)): ${options[i]}" "$((max_len - ${#options[i]}))"
+        [[ -n "${options[i + 2]}" ]] && printf "$((i / 2 + 2)): ${options[i + 2]}"
+        echo -e "\n"
+    done
+
+    _blue "q: 退出  b: 返回  0: 首页"
+    echo
+    read -ep "请输入命令号: " number
+
+    case "$number" in
+    [1-$((num_options / 2))])
+        local action_index=$((2 * (number - 1) + 1))
+        parentfun=${options[action_index]}
+        ${options[action_index]}
+        waitinput
+        main
+        ;;
+    0) main ;;
+    b) ${FUNCNAME[3]} ;;
+    q) exit ;;
+    *)
+        _red '输入错误'
+        waitinput
+        main
+        ;;
+    esac
+}
